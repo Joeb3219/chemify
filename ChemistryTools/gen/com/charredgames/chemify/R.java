fe738301101968bf0b65995ac5423d27cb3354a7
joeb3219 <joe@websitebegin.com>
2014-02-13 22:13:50 -0500

    Updated Settings tab to an actual icon.

--- ChemistryTools/gen/com/charredgames/chemify/R.java
+++ ChemistryTools/gen/com/charredgames/chemify/R.java
@@ -46,7 +46,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_action_send=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_settings=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int about_about_chemify=0x7f0b0004;

cdec41d4beb3c5872e2eafec715f3e222abc0969
joeb3219 <joe@websitebegin.com>
2014-02-18 20:22:38 -0500

    String changes; Upgrading to v 1.4.0, first public release.

--- ChemistryTools/src/com/charredgames/chemify/activity/MainActivity.java
+++ ChemistryTools/src/com/charredgames/chemify/activity/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;

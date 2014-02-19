6048deceb0cb8e87f86a4be872e063ec9ab0ee28
joeb3219 <joe@websitebegin.com>
2014-02-19 15:20:20 -0500

    Corrected the formulas of chromate and dichromate in the polyions list.

--- ChemistryTools/src/com/charredgames/chemify/Controller.java
+++ ChemistryTools/src/com/charredgames/chemify/Controller.java
@@ -43,7 +43,7 @@ import com.charredgames.chemify.problems.ResponseType;
  */
 public class Controller {
 
-	public static final String _VERSION = "1.4.0";
+	public static final String _VERSION = "1.4.1";
 	private static AssetManager assets = null;
 	public static ArrayList<ResponseType> types = new ArrayList<ResponseType>();
 	public static ArrayList<Prefix> prefixes = new ArrayList<Prefix>();

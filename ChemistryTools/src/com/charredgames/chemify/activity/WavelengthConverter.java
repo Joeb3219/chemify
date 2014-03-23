d86b8426f0054633c11fb8e824692e1e00860c6b
joeb3219 <joe@websitebegin.com>
2014-03-23 09:08:50 -0400

    Added/updated WavelengthConverter

--- ChemistryTools/src/com/charredgames/chemify/activity/WavelengthConverter.java
+++ ChemistryTools/src/com/charredgames/chemify/activity/WavelengthConverter.java
@@ -16,6 +16,7 @@ public class WavelengthConverter extends Activity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.wavelengthconverter);
+		setTitle(Controller.resources.getString(R.string.problem_waves));
 	}
 
 	public void onStart(){
@@ -61,7 +62,7 @@ public class WavelengthConverter extends Activity{
 	private void calculate(String val, String edited){
 		if(!canCalculate) return;
 		canCalculate = false;
-		double planck = 6.63 * Math.pow(10, -34);
+		double planck = 6.626 * Math.pow(10, -34);
 		double light = 3.0 * Math.pow(10, 8);
 		
 		double value = 1.00;
@@ -73,13 +74,11 @@ public class WavelengthConverter extends Activity{
 		double wavelength = 0.00;
 		if(edited.equalsIgnoreCase("wavelength")) wavelength = value;
 		else if(edited.equalsIgnoreCase("frequency")) wavelength = light / value;
-		else if(edited.equalsIgnoreCase("energy")) wavelength = light / (value
-				)
+		else if(edited.equalsIgnoreCase("energy")) wavelength = 1 / ((value) / (light * planck));
 		
-		
-		if(!edited.equalsIgnoreCase("ph")) ((EditText) findViewById(R.id.value_ph)).setText(Controller.checkInfinityValues(wavelength) + "");
-		if(!edited.equalsIgnoreCase("[h]")) ((EditText) findViewById(R.id.value_ph_concentration)).setText(Controller.checkInfinityValues(Math.pow(10, -1 * wavelength)) + "");
-		if(!edited.equalsIgnoreCase("poh")) ((EditText) findViewById(R.id.value_poh)).setText(Controller.checkInfinityValues((14.00 - wavelength)) + "");
+		if(!edited.equalsIgnoreCase("wavelength")) ((EditText) findViewById(R.id.value_wavelength)).setText(Controller.checkInfinityValues(wavelength) + "");
+		if(!edited.equalsIgnoreCase("frequency")) ((EditText) findViewById(R.id.value_frequency)).setText(Controller.checkInfinityValues(light / wavelength) + "");
+		if(!edited.equalsIgnoreCase("energy")) ((EditText) findViewById(R.id.value_energy)).setText(Controller.checkInfinityValues( (planck * light) / (wavelength)) + "");
 		
 		canCalculate = true;
 	}
